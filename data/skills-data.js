3b041eba33698453f73388a3f2e8609845855ddf
Savienne <Saviennes.mitchell@yahoo.com>
2022-06-15 14:53:21 -0400

    finish pt1

---
+++ data/skills-data.js
@@ -0,0 +1,11 @@
+const skills = [
+
+    {text: 'Fast typing',  done:true},
+    {text: 'Knowing CSS', done:true},
+    {text: 'Knowing Javascript', done: true},
+    {text: 'Knowing Javascriptt', done: true},
+]
+
+export {
+    skills  
+}
